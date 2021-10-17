d6f04bb4bf8a79f9c7cae2fef6650d7e565e02ff
j-vanrav <j-vanrav@users.noreply.github.com>
2021-10-17 11:31:42 +0800

    hit button counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from "react";
 import './App.css';
 
 function App() {
+  const APP_ID = 'fec05e16'
+  const APP_KEY = '59a5a32c19fbd4fc51f5d752b0b7b157'
+  const exampleReq= `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() =>{})
+  console.log('Effect has been run');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <h1 onClick={() => setCounter(counter+1)}>{counter}</h1>
     </div>
   );
 }
