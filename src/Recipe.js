312f1148a1f0a80c36c453b151d381d2a199274a
j-vanrav <j-vanrav@users.noreply.github.com>
2021-10-26 21:43:08 +0800

    prototype card flipping

--- src/Recipe.js
+++ src/Recipe.js
@@ -2,17 +2,23 @@ import React from 'react';
 import style from './recipe.module.css';
 
 const Recipe = ({title, calories, image, ingredients}) => {
+    calories = calories-(calories%1)
     return(
         <div className={style.recipe}>
-            <h1>{title}</h1>
-            <ol>
-                {ingredients.map(ingredient=>(
-                    <li>{ingredient.text}</li>
-                ))}
-            </ol>
-            <p>{calories}</p>
-            <img className={style.image} src={image} alt=""/>
-
+            <div className={style.recipe_inner}>
+                <div className={style.recipe_front}>
+                    <h1>{title}</h1>
+                    <img className={style.image} src={image} alt=""/>
+                </div>
+                <div className={style.recipe_back}>
+                    <ol>
+                        {ingredients.map(ingredient=>(
+                            <li>{ingredient.text}</li>
+                        ))}
+                    </ol>
+                    <p>Calories: {calories}</p>
+                </div>
+            </div>
         </div>
 
     );
