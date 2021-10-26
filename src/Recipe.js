4b469b0969e4c71376e21ebc5385b288b893eb18
j-vanrav <j-vanrav@users.noreply.github.com>
2021-10-26 22:12:40 +0800

    fix calories

--- src/Recipe.js
+++ src/Recipe.js
@@ -2,7 +2,7 @@ import React from 'react';
 import style from './recipe.module.css';
 
 const Recipe = ({title, calories, image, ingredients}) => {
-    calories = calories = (calories%1);
+    calories = calories - (calories%1);
     return(
         <div className={style.recipe}>
             <h1>{title}</h1>
@@ -11,7 +11,7 @@ const Recipe = ({title, calories, image, ingredients}) => {
                     <li>{ingredient.text}</li>
                 ))}
             </ol>
-            <p>{calories}</p>
+            <p>Calories: {calories}</p>
             <img className={style.image} src={image} alt=""/>
 
         </div>
