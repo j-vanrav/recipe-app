d2a5714b01d685405707b938bd8625528657cc87
WeeJimmyy <j.vanrav2@gmail.com>
2021-10-28 16:30:55 +0800

    in progress

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import style from './recipe.module.css';
 
-const Recipe = ({title, calories, image, ingredients}) => {
+const Recipe = ({id, title, calories, image, ingredients}) => {
     calories = calories - (calories%1);
     return(
-        <div className={style.recipe}>
-            <h1>{title}</h1>
-            <ol>
+        <div className={style.recipeHolder}>
+            <input id="collapsible" className={style.toggle} type="checkbox"/>
+            <label for="collapsible" className={style.recipe}>
+                <h1>{title}</h1>
+                <p>Calories: {calories}</p>
+                <img className={style.image} src={image} alt=""/>
+            </label>
+            <div className={style.ingredients}>
+            <ul>
                 {ingredients.map(ingredient=>(
                     <li>{ingredient.text}</li>
                 ))}
-            </ol>
-            <p>Calories: {calories}</p>
-            <img className={style.image} src={image} alt=""/>
-
+            </ul>
+            </div>
         </div>
+        
+
 
     );
 }
